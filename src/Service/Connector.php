aa2518234d62a49326f76ce8162cd50da8854fc9
caseyfw <casey.fulton@adelaide.edu.au>
2017-07-20 15:10:14 +0930

    Filled out getRolesForUser() method, fixed config.

--- src/Service/Connector.php
+++ src/Service/Connector.php
@@ -3,6 +3,8 @@
 namespace Drupal\shepherd_connector\Service;
 
 use Drupal\Core\Site\Settings;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 
 /**
  * Provides a mechanism for communicating with Shepherd.
@@ -12,13 +14,41 @@ use Drupal\Core\Site\Settings;
 class Connector {
 
   /**
+   * The settings instance.
+   *
+   * @var \Drupal\Core\Site\Settings
+   *   The settings instance.
+   */
+  protected $settings;
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Constructs a new Connector object.
+   *
+   * @param \Drupal\Core\Site\Settings $settings
+   *   The settings instance.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   A Guzzle client object.
+   */
+  public function __construct(Settings $settings, ClientInterface $http_client) {
+    $this->settings = $settings;
+    $this->httpClient = $http_client;
+  }
+
+  /**
    * Returns the url for Shepherd configured in settings.php.
    *
    * @return string
    *   The url for Shepherd, or NULL if not set.
    */
   public function getUrl() {
-    return Settings::get('shepherd_url');
+    return $this->settings->get('shepherd_url');
   }
 
   /**
@@ -28,7 +58,7 @@ class Connector {
    *   The token for Shepherd, or NULL if not set.
    */
   public function getToken() {
-    return Settings::get('shepherd_token');
+    return $this->settings->get('shepherd_token');
   }
 
   /**
@@ -38,7 +68,7 @@ class Connector {
    *   The site id for this site according to Shepherd, or NULL if not set.
    */
   public function getSiteId() {
-    return Settings::get('site_id');
+    return $this->settings->get('shepherd_site_id');
   }
 
   /**
@@ -52,8 +82,22 @@ class Connector {
    *   roles.
    */
   public function getRolesForUser(string $username) {
-    // @todo Actually make this query Shepherd for roles.
-    return ['administrator'];
+    try {
+      $site_user_roles_url = $this->getUrl() . '/api/site-user-roles/' . $username . '/' . $this->getSiteId();
+      $options = [];
+      if ($this->getToken()) {
+        $options = [
+          'headers' => [
+            'Authorization' => 'Bearer ' . $this->getToken(),
+          ],
+        ];
+      }
+      $response = $this->httpClient->request('GET', $site_user_roles_url, $options);
+      return json_decode($response->getBody(), TRUE);
+    }
+    catch (GuzzleException $exception) {
+      return [];
+    }
   }
 
   /**
